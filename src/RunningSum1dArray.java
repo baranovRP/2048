c4e7e9aca4ffc5f72b6c63ec0a275678449532a1
baranovrp <baranov.rp@gmail.com>
2021-09-28 20:56:31 +0200

    LEETCODE-1480 Running Sum of 1d Array

---
+++ src/RunningSum1dArray.java
@@ -0,0 +1,10 @@
+class RunningSum1dArray {
+    public int[] runningSum(int[] nums) {
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            nums[i] += nums[i - 1];
+
+        }
+        return nums;
+    }
+}
