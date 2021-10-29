7d3eb1492f161e9b6f7d5bd7d854aac388bc7b75
baranovrp <baranov.rp@gmail.com>
2021-10-29 20:59:16 +0200

    Grokking-2.8 Subarrays with Product Less than a Target (medium)
    Calculate complexity

--- src/sliding_window/SubarrayProductLessThanK.java
+++ src/sliding_window/SubarrayProductLessThanK.java
@@ -31,4 +31,40 @@ class SubarrayProductLessThanK {
         System.out.println(SubarrayProductLessThanK.findSubarrays(new int[]{2, 5, 3, 10}, 30));
         System.out.println(SubarrayProductLessThanK.findSubarrays(new int[]{8, 2, 6, 5}, 50));
     }
+    /**
+     * Time complexity
+     * The main for-loop managing the sliding window takes O(N)
+     * but creating subarrays can take up to O(N^2) in the worst case.
+     * Therefore overall, our algorithm will take O(N^3).
+     *
+     * Space complexity
+     * Ignoring the space required for the output list, the algorithm runs in O(N) space which is used for the temp list.
+     *
+     * Can you try estimating how much space will be required for the output list?
+     * The worst-case will happen when every subarray has a product less than the target!
+     * So the question will be, how many contiguous subarrays an array can have?
+     * It is definitely not all Permutations of the given array; is it all Combinations of the given array?
+     * It is not all the Combinations of all elements of the array!
+     * For an array with distinct elements,
+     * finding all of its contiguous subarrays is like finding the number of ways to choose two indices,
+     * i and j, in the array such that i <= j.
+     *
+     * If there are a total of n elements in the array, here is how we can count all the contiguous subarrays:
+     *
+     *     When i = 0, j can have any value from 0 to n-1, giving a total of n choices.
+     *     When i = 1, j can have any value from 1 to n-1, giving a total of n-1 choices.
+     *     Similarly, when i = 2, j can have n-2 choices.
+     *     …
+     *     …
+     *     When i = n-1, j can only have only 1 choice.
+     *
+     * Let’s combine all the choices:
+     *
+     *     n + (n-1) + (n-2) + ... 3 + 2 + 1
+     *
+     * Which gives us a total of: n∗(n+1)/2
+     *
+     * So, at most, we need space for O(n^2) output lists.
+     * At worst, each subarray can take O(n) space, so overall, our algorithm’s space complexity will be O(n^3).
+     */
 }
