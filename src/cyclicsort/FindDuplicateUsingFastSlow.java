a71d609bbc69daebd265c13e3212054c81dbc2c9
baranovrp <baranov.r.p@gmail.com>
2021-11-17 20:46:11 +0100

    Grokking-5.05 Find the Duplicate Number using fast&slow approach(easy)

---
+++ src/cyclicsort/FindDuplicateUsingFastSlow.java
@@ -0,0 +1,52 @@
+package cyclicsort;
+
+class FindDuplicateUsingFastSlow {
+
+    /**
+     * Time complexity
+     * The time complexity of the above algorithm is O(n).
+     * <p>
+     * Space complexity
+     * The algorithm runs in constant space O(1) but modifies the input array.
+     */
+    public static int findDuplicate(int[] arr) {
+        int slow = 0, fast = 0;
+        do {
+            slow = arr[slow];
+            fast = arr[arr[fast]];
+        } while (slow != fast);
+
+        // find cycle length
+        int current = arr[slow];
+        int cycleLength = 0;
+        do {
+            current = arr[current];
+            cycleLength++;
+        } while (current != arr[slow]);
+
+        return findStart(arr, cycleLength);
+    }
+
+    private static int findStart(int[] arr, int cycleLength) {
+        int pointer1 = arr[0], pointer2 = arr[0];
+        // move pointer2 ahead 'cycleLength' steps
+        while (cycleLength > 0) {
+            pointer2 = arr[pointer2];
+            cycleLength--;
+        }
+
+        // increment both pointers until they meet at the start of the cycle
+        while (pointer1 != pointer2) {
+            pointer1 = arr[pointer1];
+            pointer2 = arr[pointer2];
+        }
+
+        return pointer1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(FindDuplicateUsingFastSlow.findDuplicate(new int[]{1, 4, 4, 3, 2}));
+        System.out.println(FindDuplicateUsingFastSlow.findDuplicate(new int[]{2, 1, 3, 3, 5, 4}));
+        System.out.println(FindDuplicateUsingFastSlow.findDuplicate(new int[]{2, 4, 1, 4, 4}));
+    }
+}
