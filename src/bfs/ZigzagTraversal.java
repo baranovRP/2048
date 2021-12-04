d261ddaa0efc0b35772d875fd026f1bcce80e9aa
baranovrp <baranov.r.p@gmail.com>
2021-12-04 11:59:32 +0100

    Grokking-7.03 Zigzag Traversal (medium)

---
+++ src/bfs/ZigzagTraversal.java
@@ -0,0 +1,93 @@
+package bfs;
+
+import java.util.*;
+
+class ZigzagTraversal {
+    public static List<List<Integer>> traverse(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean leftToRight = true;
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode current = queue.poll();
+
+                if (leftToRight) currentLevel.add(current.val);
+                else currentLevel.add(0, current.val);
+
+                if (current.left != null) queue.offer(current.left);
+                if (current.right != null) queue.offer(current.right);
+
+            }
+            result.add(currentLevel);
+            leftToRight = !leftToRight;
+        }
+        return result;
+    }
+
+    /**
+     * Time complexity
+     * The time complexity of the above algorithm is O(N),
+     * where ‘N’ is the total number of nodes in the tree.
+     * This is due to the fact that we traverse each node once.
+     * <p>
+     * Space complexity
+     * The space complexity of the above algorithm will be O(N)
+     * as we need to return a list containing the level order traversal.
+     * We will also need O(N) space for the queue.
+     * Since we can have a maximum of N/2 nodes at any level (this could happen only at the lowest level),
+     * therefore we will need O(N) space to store them in the queue.
+     */
+    public static List<List<Integer>> traverse2(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null)
+            return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean leftToRight = true;
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new LinkedList<>();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+
+                // add the node to the current level based on the traverse direction
+                if (leftToRight)
+                    currentLevel.add(currentNode.val);
+                else
+                    currentLevel.add(0, currentNode.val);
+
+                // insert the children of current node in the queue
+                if (currentNode.left != null)
+                    queue.offer(currentNode.left);
+                if (currentNode.right != null)
+                    queue.offer(currentNode.right);
+            }
+            result.add(currentLevel);
+            // reverse the traversal direction
+            leftToRight = !leftToRight;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(12);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(9);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(5);
+        root.right.left.left = new TreeNode(20);
+        root.right.left.right = new TreeNode(17);
+        List<List<Integer>> result = ZigzagTraversal.traverse(root);
+        System.out.println("Zigzag traversal: " + result);
+    }
+}
+
