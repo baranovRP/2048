5d8c1d78b89de0277b9abb26cbc92ea693fec41f
baranovrp <baranov.r.p@gmail.com>
2021-12-14 20:07:05 +0100

    Grokking-8.06 Tree Diameter (medium)

---
+++ src/dfs/TreeDiameter.java
@@ -0,0 +1,64 @@
+package dfs;
+
+class TreeDiameter {
+
+    private static int treeDiameter = 0;
+
+    /**
+     * Time complexity
+     * The time complexity of the algorithm is O(N),
+     * where ‘N’ is the total number of nodes in the tree.
+     * This is due to the fact that we traverse each node once.
+     * <p>
+     * Space complexity
+     * The space complexity of the algorithm will be O(N) in the worst case.
+     * This space will be used to store the recursion stack.
+     * The worst case will happen when the given tree is a linked list (i.e., every node has only one child).
+     */
+    public static int findDiameter(TreeNode root) {
+        calculateHeight(root);
+        return treeDiameter;
+    }
+
+    private static int calculateHeight(TreeNode currentNode) {
+        if (currentNode == null)
+            return 0;
+
+        int leftTreeHeight = calculateHeight(currentNode.left);
+        int rightTreeHeight = calculateHeight(currentNode.right);
+
+        // if the current node doesn't have a left or right subtree, we can't have
+        // a path passing through it, since we need a leaf node on each side
+        if (leftTreeHeight != 0 && rightTreeHeight != 0) {
+
+            // diameter at the current node will be equal to the height of left subtree +
+            // the height of right sub-trees + '1' for the current node
+            int diameter = leftTreeHeight + rightTreeHeight + 1;
+
+            // update the global tree diameter
+            treeDiameter = Math.max(treeDiameter, diameter);
+        }
+
+        // height of the current node will be equal to the maximum of the heights of
+        // left or right subtrees plus '1' for the current node
+        return Math.max(leftTreeHeight, rightTreeHeight) + 1;
+    }
+
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+        System.out.println("Tree Diameter: " + TreeDiameter.findDiameter(root));
+        root.left.left = null;
+        root.right.left.left = new TreeNode(7);
+        root.right.left.right = new TreeNode(8);
+        root.right.right.left = new TreeNode(9);
+        root.right.left.right.left = new TreeNode(10);
+        root.right.right.left.left = new TreeNode(11);
+        System.out.println("Tree Diameter: " + TreeDiameter.findDiameter(root));
+    }
+}
