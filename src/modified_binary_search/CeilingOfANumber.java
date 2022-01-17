53e7fca1ff2a0dc9cfeee892b3d74c7b8e42c511
baranovrp <baranov.r.p@gmail.com>
2022-01-17 18:55:49 +0100

    Grokking-11.02 Ceiling of a Number (medium)

---
+++ src/modified_binary_search/CeilingOfANumber.java
@@ -0,0 +1,40 @@
+package modified_binary_search;
+
+class CeilingOfANumber {
+
+    /**
+     * Time complexity
+     * Since we are reducing the search range by half at every step,
+     * this means that the time complexity of our algorithm will be O(logN)
+     * where ‘N’ is the total elements in the given array.
+     * <p>
+     * Space complexity
+     * The algorithm runs in constant space O(1).
+     */
+    public static int searchCeilingOfANumber(int[] arr, int key) {
+        if (key > arr[arr.length - 1]) // if the 'key' is bigger than the biggest element
+            return -1;
+
+        int start = 0, end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (key < arr[mid]) {
+                end = mid - 1;
+            } else if (key > arr[mid]) {
+                start = mid + 1;
+            } else { // found the key
+                return mid;
+            }
+        }
+        // since the loop is running until 'start <= end', so at the end of the while loop, 'start == end+1'
+        // we are not able to find the element in the given array, so the next big number will be arr[start]
+        return start;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(CeilingOfANumber.searchCeilingOfANumber(new int[]{4, 6, 10}, 6));
+        System.out.println(CeilingOfANumber.searchCeilingOfANumber(new int[]{1, 3, 8, 10, 15}, 12));
+        System.out.println(CeilingOfANumber.searchCeilingOfANumber(new int[]{4, 6, 10}, 17));
+        System.out.println(CeilingOfANumber.searchCeilingOfANumber(new int[]{4, 6, 10}, -1));
+    }
+}
