c1fc20e15ca9ebc74f8fccb392b71c4f581b902a
baranovrp <baranov.r.p@gmail.com>
2022-01-21 19:25:10 +0100

    Grokking-11.07 Minimum Difference Element (medium)

---
+++ src/modified_binary_search/MinimumDifference.java
@@ -0,0 +1,46 @@
+package modified_binary_search;
+
+class MinimumDifference {
+
+    /**
+     * Time complexity
+     * Since, we are reducing the search range by half at every step,
+     * this means the time complexity of our algorithm will be O(logN)
+     * where ‘N’ is the total elements in the given array.
+     * <p>
+     * Space complexity
+     * The algorithm runs in constant space O(1).
+     */
+    public static int searchMinDiffElement(int[] arr, int key) {
+        if (key < arr[0])
+            return arr[0];
+        if (key > arr[arr.length - 1])
+            return arr[arr.length - 1];
+
+        int start = 0, end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (key < arr[mid]) {
+                end = mid - 1;
+            } else if (key > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return arr[mid];
+            }
+        }
+
+        // at the end of the while loop, 'start == end+1'
+        // we are not able to find the element in the given array
+        // return the element which is closest to the 'key'
+        if ((arr[start] - key) < (key - arr[end]))
+            return arr[start];
+        return arr[end];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MinimumDifference.searchMinDiffElement(new int[]{4, 6, 10}, 7));
+        System.out.println(MinimumDifference.searchMinDiffElement(new int[]{4, 6, 10}, 4));
+        System.out.println(MinimumDifference.searchMinDiffElement(new int[]{1, 3, 8, 10, 15}, 12));
+        System.out.println(MinimumDifference.searchMinDiffElement(new int[]{4, 6, 10}, 17));
+    }
+}
