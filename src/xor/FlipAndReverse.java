b4e561f4bc26cd577433151c58baa2f4526ed9d1
baranovrp <baranov.r.p@gmail.com>
2022-02-05 20:30:39 +0100

    Grokking-12.05 Problem Statement, Flip and Reverse (hard)

---
+++ src/xor/FlipAndReverse.java
@@ -0,0 +1,57 @@
+package xor;
+
+/**
+ * Input: [             Input: [
+ *   [1,0,1],             [1,1,0,0],
+ *   [1,1,1],             [1,0,0,1],
+ *   [0,1,1]              [0,1,1,1],
+ * ]                      [1,0,1,0]
+ * Output: [            ]
+ *   [0,1,0],           Output: [
+ *   [0,0,0],             [1,1,0,0],
+ *   [0,0,1]              [0,1,1,0],
+ * ]                      [0,0,0,1],
+ *                        [1,0,1,0]
+ *                      ]
+ */
+
+public class FlipAndReverse {
+
+    /**
+     * Time Complexity
+     * The time complexity of this solution is O(n) as we iterate through all elements of the input.
+     * <p>
+     * Space Complexity
+     * The space complexity of this solution is O(1).
+     */
+    public static int[][] flipAndInvertImage(int[][] arr) {
+        int C = arr[0].length;
+        for (int[] row : arr) {
+            for (int i = 0; i < (C + 1) / 2; ++i) {
+                int tmp = row[i] ^ 1;
+                row[i] = row[C - 1 - i] ^ 1;
+                row[C - 1 - i] = tmp;
+            }
+        }
+        return arr;
+    }
+
+    public static void print(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {{1, 0, 1}, {1, 1, 1}, {0, 1, 1}};
+        print(flipAndInvertImage(arr));
+        System.out.println();
+
+        int[][] arr2 = {{1, 1, 0, 0}, {1, 0, 0, 1}, {0, 1, 1, 1}, {1, 0, 1, 0}};
+        print(flipAndInvertImage(arr2));
+    }
+}
