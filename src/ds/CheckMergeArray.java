5d66d569afd03ef5b511abf0b7324630c694789d
baranovrp <baranov.r.p@gmail.com>
2022-02-08 21:18:15 +0100

    DS-Array CheckMergeArray, CheckRemoveEven

---
+++ src/ds/CheckMergeArray.java
@@ -0,0 +1,50 @@
+package ds;
+
+public class CheckMergeArray {
+
+    /**
+     * Time Complexity
+     * The time complexity for this algorithm is O(n+m),
+     * where nnn and mmm are the sizes of arr1 and arr2, respectively.
+     * This is because both arrays are iterated over once.
+     * <p>
+     * Space Complexity
+     * O(n+m)
+     */
+    public static int[] mergeArrays(int[] arr1, int[] arr2) {
+        int len1 = arr1.length;
+        int len2 = arr2.length;
+        int[] res = new int[len1 + len2];
+        int idx1 = 0, idx2 = 0;
+
+        for (int i = 0; i < res.length; i++) {
+            if (idx1 >= len1) {
+                res[i] = arr2[idx2];
+                idx2++;
+            } else if (idx2 >= len2) {
+                res[i] = arr1[idx1];
+                idx1++;
+            } else if (arr1[idx1] < arr2[idx2]) {
+                res[i] = arr1[idx1];
+                idx1++;
+            } else {
+                res[i] = arr2[idx2];
+                idx2++;
+            }
+        }
+        return res; // make a new resultant array and return your results in that
+    }
+
+    public static void main(String args[]) {
+
+        int[] arr1 = {1, 12, 14, 17, 23}; // creating a sorted array called arr1
+        int[] arr2 = {11, 19, 27};  // creating a sorted array called arr2
+
+        int[] resultantArray = mergeArrays(arr1, arr2); // calling mergeArrays
+
+        System.out.print("Arrays after merging: ");
+        for (int i = 0; i < arr1.length + arr2.length; i++) {
+            System.out.print(resultantArray[i] + " ");
+        }
+    }
+}
---
+++ src/ds/CheckRemoveEven.java
@@ -0,0 +1,44 @@
+package ds;
+
+public class CheckRemoveEven {
+
+    /**
+     * Time Complexity
+     * Since the entire array has to be iterated over, this solution is in O(n).
+     * <p>
+     * Space Complexity
+     * O(n)
+     */
+    public static int[] removeEven(int[] arr) {
+        int start = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                arr[start] = arr[i];
+                start++;
+            }
+        }
+        int[] res = new int[start];
+        for (int i = 0; i < res.length; i++) res[i] = arr[i];
+        return res; // change this and return the correct result array
+    }
+
+    public static void main(String args[]) {
+
+        int size = 10;
+        int[] arr = new int[size]; //declaration and instantiation
+
+        System.out.print("Before removing Even Numbers: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i; // assigning values
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        int[] newArr = removeEven(arr); // calling removeEven
+
+        System.out.print("After removing Even Numbers: ");
+        for (int i = 0; i < newArr.length; i++) {
+            System.out.print(newArr[i] + " "); // prinitng array
+        }
+    }
+}
