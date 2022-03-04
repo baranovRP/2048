d6a64861daf2fc3074ed0620b6dec2d7811da604
baranovrp <baranov.r.p@gmail.com>
2022-03-04 20:23:12 +0100

    DS-Stack Implement Two Stacks using one Array

---
+++ src/ds/stack_queue/TwoStacks.java
@@ -0,0 +1,56 @@
+package ds.stack_queue;
+
+//You can either divide array in two halves or start stacks at extreme ends.
+//We'll use the second technique to solve this problem.
+//Top of Stack 1 start from extreme left of array i.e top1 = 0;
+//Top of Stack 2 start from extreme right of array i.e top2 = size - 1
+
+/**
+ * Time Complexity
+ * All the operations take constant time, i.e.,
+ * O(1) because the array is being indexed and not resized.
+ */
+public class TwoStacks<V> {
+    private int maxSize;
+    private int top1, top2; //Store top value indices of Stack 1 and Stack 2
+    private V[] array;
+
+    @SuppressWarnings("unchecked")
+    public TwoStacks(int max_size) {
+        this.maxSize = max_size;
+        this.top1 = -1;
+        this.top2 = max_size;
+        array = (V[]) new Object[max_size];//type casting Object[] to V[]
+    }
+
+    //insert at top of first stack
+    public void push1(V value) {
+        if (top1 < top2 - 1) {
+            array[++top1] = value;
+        }
+    }
+
+    //insert at top of second stack
+    public void push2(V value) {
+        if (top1 < top2 - 1) {
+            array[--top2] = value;
+        }
+    }
+
+    //remove and return value from top of first stack
+    public V pop1() {
+        if (top1 > -1) {
+            return array[top1--];
+        }
+        return null;
+    }
+
+    //remove and return value from top of second stack
+    public V pop2() {
+        if (top2 < maxSize) {
+            return array[top2++];
+        }
+        return null;
+    }
+}
+
