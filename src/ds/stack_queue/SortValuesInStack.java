3ed4ce96f28a18fd24786f000da1ad3e14577cb8
baranovrp <baranov.r.p@gmail.com>
2022-03-06 14:43:56 +0100

    DS-Stack Sort the Values in a Stack

---
+++ src/ds/stack_queue/SortValuesInStack.java
@@ -0,0 +1,49 @@
+package ds.stack_queue;
+
+public class SortValuesInStack {
+
+    public static void sortStack(Stack<Integer> stack) {
+        //1. Use a second tempStack.
+        //2. Pop value from mainStack.
+        //3. If the value is greater or equal to the top of tempStack, then push the value in tempStack
+        //else pop all values from tempStack and push them in mainStack and in the end push value in tempStack and repeat from step 2.
+        //till mainStack is not empty.
+        //4. When mainStack will be empty, tempStack will have sorted values in descending order.
+        //5. Now transfer values from tempStack to mainStack to make values sorted in ascending order.
+
+        /**
+         * Time Complexity
+         * The outer and inner loops both traverse all the n elements of the stack.
+         * Hence, the time complexity is O(n^2).
+         */
+        Stack<Integer> newStack = new Stack<>(stack.getMaxSize());
+        while (!stack.isEmpty()) {
+            Integer value = stack.pop();
+            if (!newStack.isEmpty() && value >= newStack.top()) {
+                newStack.push(value);
+            } else {
+                while (!newStack.isEmpty() && newStack.top() > value)
+                    stack.push(newStack.pop());
+                newStack.push(value);
+            }
+        }
+        while (!newStack.isEmpty())
+            stack.push(newStack.pop());
+    }
+
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack<Integer>(7);
+        stack.push(2);
+        stack.push(97);
+        stack.push(4);
+        stack.push(42);
+        stack.push(12);
+        stack.push(60);
+        stack.push(23);
+        sortStack(stack);
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
+}
