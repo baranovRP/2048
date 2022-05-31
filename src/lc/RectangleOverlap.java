d0d63a1f390bd0166f463ec79318ce6fc32ba25f
baranovRP <baranov.r.p@gmail.com>
2022-05-31 21:54:48 +0200

    lc-836 Rectangle Overlap

---
+++ src/lc/RectangleOverlap.java
@@ -0,0 +1,27 @@
+package lc;
+
+public class RectangleOverlap {
+
+    // Time = O(1)
+    // Space = O(1)
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        // check if either rectangle is actually a line
+        if (rec1[0] == rec1[2] || rec1[1] == rec1[3] ||
+                rec2[0] == rec2[2] || rec2[1] == rec2[3]) {
+            // the line cannot have positive overlap
+            return false;
+        }
+
+        return !(rec1[2] <= rec2[0] ||   // left
+                rec1[3] <= rec2[1] ||   // bottom
+                rec1[0] >= rec2[2] ||   // right
+                rec1[1] >= rec2[3]);    // top
+    }
+
+    // Time = O(1)
+    // Space = O(1)
+    public boolean isRectangleOverlap1(int[] rec1, int[] rec2) {
+        return (Math.min(rec1[2], rec2[2]) > Math.max(rec1[0], rec2[0]) && // width > 0
+                Math.min(rec1[3], rec2[3]) > Math.max(rec1[1], rec2[1]));  // height > 0
+    }
+}
