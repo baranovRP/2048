0152e14bcd1b87e7442d512850ccdd3f9b7da252
baranovRP <baranov.r.p@gmail.com>
2022-06-02 16:11:39 +0200

    lc-860 Lemonade Change

--- src/lc/LemonadeChange.java
+++ src/lc/LemonadeChange.java
@@ -3,8 +3,55 @@ package lc;
 public class LemonadeChange {
 
     public boolean lemonadeChange(int[] bills) {
-        int sum=0;
-        int[] nominals = new int[]{0,0,0};
+        int[] nominals = new int[]{0, 0, 0};
+        for (int i = 0; i < bills.length; i++) {
+            if (bills[i] == 20) {
+                if (nominals[1] >= 1 && nominals[0] >= 1) {
+                    nominals[1] -= 1;
+                    nominals[0] -= 1;
+                    nominals[2] += 1;
+                } else if (nominals[0] >= 3) {
+                    nominals[0] -= 3;
+                } else {
+                    return false;
+                }
+            } else if (bills[i] == 10) {
+                if (nominals[0] >= 1) {
+                    nominals[0] -= 1;
+                    nominals[1] += 1;
+                } else {
+                    return false;
+                }
+            } else {
+                nominals[0] += 1;
+            }
+        }
+        return true;
+    }
 
+    // Time = O(N)
+    // Space = O(1)
+    public boolean lemonadeChange1(int[] bills) {
+        int five = 0, ten = 0;
+        for (int bill : bills) {
+            if (bill == 5)
+                five++;
+            else if (bill == 10) {
+                if (five == 0) return false;
+                five--;
+                ten++;
+            } else {
+                if (five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
