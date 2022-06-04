fb12037bc9259b4a17afd30a43a77044d9f2d4de
baranovRP <baranov.r.p@gmail.com>
2022-06-04 16:51:30 +0200

    lc-414 Third Maximum Number

--- src/lc/ThirdMaximumNumber.java
+++ src/lc/ThirdMaximumNumber.java
@@ -2,7 +2,6 @@ package lc;
 
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.PriorityQueue;
 import java.util.Set;
 
 public class ThirdMaximumNumber {
@@ -66,9 +65,32 @@ public class ThirdMaximumNumber {
         return counter == 3 ? trdMax : fstMax;
     }
 
+    public int thirdMax1(int[] nums) {
+        Integer fm = null, sm = null, tm = null;
+        for (int num : nums) {
+            if (fm == null || num > fm) {
+                tm = sm; sm = fm; fm = num;
+            } else if (num == fm) {
+                continue;
+            } else if (sm == null || num > sm) {
+                tm = sm; sm = num;
+            } else if (num == sm) {
+                continue;
+            } else if (tm == null || num > tm) {
+                tm = num;
+            }
+        }
+        if (tm != null) {
+            return tm;
+        } else {
+            return fm;
+        }
+    }
+
+
     // Time = O(N)
     // Space = O(1)
-    public int thirdMax1(int[] nums) {
+    public int thirdMax2(int[] nums) {
         Set<Integer> maximums = new HashSet<Integer>();
         for (int num : nums) {
             maximums.add(num);
@@ -81,7 +103,7 @@ public class ThirdMaximumNumber {
 
     public static void main(String[] args) {
         ThirdMaximumNumber tmn = new ThirdMaximumNumber();
-        System.out.println(tmn.thirdMax(new int[]{1,-2147483648,2}));
-        System.out.println(tmn.thirdMax(new int[]{3,2,1}));
+        System.out.println(tmn.thirdMax2(new int[]{1,-2147483648,2}));
+        System.out.println(tmn.thirdMax2(new int[]{3,2,1}));
     }
 }
