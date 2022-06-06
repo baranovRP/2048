1ba90bd0ef19fb8ff0dae9b428458405306afffa
baranovRP <baranov.r.p@gmail.com>
2022-06-06 21:06:58 +0200

    lc-647 Palindromic Substrings

--- src/lc/PalindromicSubstrings.java
+++ src/lc/PalindromicSubstrings.java
@@ -2,6 +2,8 @@ package lc;
 
 public class PalindromicSubstrings {
 
+    // Time = O(N^3)
+    // Space = O(1)
     public int countSubstrings(String s) {
         int counter=0;
         int start;
@@ -26,4 +28,37 @@ public class PalindromicSubstrings {
         return true;
     }
 
+    // Time = O(N^2)
+    // Space = O(1)
+    // Expand Around Possible Centers
+    public int countSubstrings1(String s) {
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); ++i) {
+            // odd-length palindromes, single character center
+            ans += countPalindromesAroundCenter(s, i, i);
+
+            // even-length palindromes, consecutive characters center
+            ans += countPalindromesAroundCenter(s, i, i + 1);
+        }
+
+        return ans;
+    }
+
+    private int countPalindromesAroundCenter(String ss, int lo, int hi) {
+        int ans = 0;
+
+        while (lo >= 0 && hi < ss.length()) {
+            if (ss.charAt(lo) != ss.charAt(hi))
+                break;      // the first and last characters don't match!
+
+            // expand around the center
+            lo--;
+            hi++;
+
+            ans++;
+        }
+
+        return ans;
+    }
 }
