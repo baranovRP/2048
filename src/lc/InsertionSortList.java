a23227c74d39012a38ecd11653a58658472e8047
baranovRP <baranov.r.p@gmail.com>
2022-10-28 20:50:02 +0200

    lc-147. Insertion Sort List

---
+++ src/lc/InsertionSortList.java
@@ -0,0 +1,48 @@
+package lc;
+
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class InsertionSortList {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode();
+        ListNode curr = head;
+
+        while (curr != null) {
+            // At each iteration, we insert an element into the resulting list.
+            ListNode prev = dummy;
+
+            // find the position to insert the current node
+            while (prev.next != null && prev.next.val <= curr.val) {
+                prev = prev.next;
+            }
+
+            ListNode next = curr.next;
+            // insert the current node to the new list
+            curr.next = prev.next;
+            prev.next = curr;
+
+            // moving on to the next iteration
+            curr = next;
+        }
+
+        return dummy.next;
+    }
+}
